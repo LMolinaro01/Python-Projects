715b829d54d0b67fb299770b0fea509f3f1ab652
Leonardo Molinaro <leomolinaro015@gmail.com>
2024-04-25 15:43:17 -0300

    Visual mais polido (Botão com cores e bem posicionados)

--- Projetos/02 - Sistema de Estoque/main.py
+++ Projetos/02 - Sistema de Estoque/main.py
@@ -10,12 +10,12 @@ def telaInicial():
     telaInicio = tkinter.Tk()
     telaInicio.title("Página Inicial")
     telaInicio.resizable(False, False)
-    telaInicio.geometry("300x500")
+    #telaInicio.geometry("300x500")
 
     label = tkinter.Label(telaInicio, text="Bem vindo ao Sistema de Estoque!")
-    label.grid(row=0, column=1, pady=10)
+    label.grid(row=0, column=2, pady=10, sticky='ew')
 
-    image1 = Image.open("image.png")
+    image1 = Image.open("logo.png")
     width, height = 200, 200
     image1.thumbnail((width, height))
     test = ImageTk.PhotoImage(image1)
@@ -23,23 +23,22 @@ def telaInicial():
     label1.image = test
     label1.grid(row=1, column=1, pady=10, padx=47)
 
-    button = tkinter.Button(telaInicio, text="",
-                            bg="#009FD6", fg="white")
-    # sticky='ew' -> estica o botao horizontalmente
-    button.grid(row=2, column=1, padx=10, pady=10, sticky='ew')
+    button = tkinter.Button(telaInicio, text="Adicionar ao Estoque",
+                            bg="#6B58FF", fg="white")
+    button.grid(row=2, column=2, padx=10, pady=10, sticky='ew')
 
     botao_prova = tkinter.Button(
-        telaInicio, text="", bg="#009FD6", fg="white")
-    botao_prova.grid(row=3, column=1, padx=10, pady=10, sticky='ew')
+        telaInicio, text="Editar Produto", bg="#3D8EF0", fg="white")
+    botao_prova.grid(row=3, column=2, padx=10, pady=10, sticky='ew')
 
     botao_tabela = tkinter.Button(
-        telaInicio, text="", bg="#009FD6", fg="white")
-    botao_tabela.grid(row=4, column=1, padx=10, pady=10, sticky='ew')
+        telaInicio, text="Exibir Estoque", bg="#1CB9E4", fg="white")
+    botao_tabela.grid(row=4, column=2, padx=10, pady=10, sticky='ew')
 
     button = tkinter.Button(telaInicio, text="Sair do Programa",
-                            bg="#009FD6", fg="white", command=lambda: telaInicio.destroy())
-    button.grid(row=5, column=1, padx=10, pady=10,
-                sticky='e')  # stiky e vai pra direita
+                            bg="#4A2ED1", fg="white", command=lambda: telaInicio.destroy())
+    button.grid(row=5, column=2, padx=100, pady=50)
 
     telaInicio.mainloop()
 
+telaInicial()
