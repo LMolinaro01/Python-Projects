56bd1b9e40dda1542dfff5d867e18c2ba5a7110d
Leonardo Molinaro <leomolinaro015@gmail.com>
2024-04-25 15:25:52 -0300

    Pequenas alterações

--- Projetos/02 - Sistema de Estoque/main.py
+++ Projetos/02 - Sistema de Estoque/main.py
@@ -15,7 +15,7 @@ def telaInicial():
     label = tkinter.Label(telaInicio, text="Bem vindo ao Sistema de Estoque!")
     label.grid(row=0, column=1, pady=10)
 
-    image1 = Image.open("logoEstacio2.png")
+    image1 = Image.open("image.png")
     width, height = 200, 200
     image1.thumbnail((width, height))
     test = ImageTk.PhotoImage(image1)
@@ -23,17 +23,17 @@ def telaInicial():
     label1.image = test
     label1.grid(row=1, column=1, pady=10, padx=47)
 
-    button = tkinter.Button(telaInicio, text="Cadastre-se",
+    button = tkinter.Button(telaInicio, text="",
                             bg="#009FD6", fg="white")
     # sticky='ew' -> estica o botao horizontalmente
     button.grid(row=2, column=1, padx=10, pady=10, sticky='ew')
 
     botao_prova = tkinter.Button(
-        telaInicio, text="Realizar Prova", bg="#009FD6", fg="white")
+        telaInicio, text="", bg="#009FD6", fg="white")
     botao_prova.grid(row=3, column=1, padx=10, pady=10, sticky='ew')
 
     botao_tabela = tkinter.Button(
-        telaInicio, text="Ver Pontuações", bg="#009FD6", fg="white")
+        telaInicio, text="", bg="#009FD6", fg="white")
     botao_tabela.grid(row=4, column=1, padx=10, pady=10, sticky='ew')
 
     button = tkinter.Button(telaInicio, text="Sair do Programa",
