4c9f2b9094f55d7cb37ec26eda45b64405ce4b72
Leonardo Molinaro <126402616+LMolinaro01@users.noreply.github.com>
2025-03-26 17:28:18 -0300

    Ex 1 - Big Data (Leitura das NF)

--- Exercícios/Big Data/Extrator NF/main.py
+++ Exercícios/Big Data/Extrator NF/main.py
@@ -1,4 +1,6 @@
 import re
+import os
+
 
 #(?=Valor Total:\s*R\$\s*([\d,]*))
 #Não pega 200.000,00
@@ -6,3 +8,17 @@ import re
 #(?<=Valor Total:\sR\$)\s+([\d]{1,3}(?:[.\d]{3})*(?:,\d{2})?)
 #Pega 200.000,00
 
+regex = r"(?<=Valor Total:\sR\$)\s+([\d]{1,3}(?:[.\d]{3})*(?:,\d{2})?)"
+
+pasta_atual = os.path.dirname(os.path.abspath(__file__))
+
+
+for arquivo in os.listdir(pasta_atual):
+    if arquivo.endswith(".txt"):
+        caminho_arquivo = os.path.join(pasta_atual, arquivo)
+        with open(caminho_arquivo, "r", encoding="utf-8") as f:
+            conteudo = f.read()
+            valores = re.findall(regex, conteudo)
+            print(f"Arquivo: {arquivo}")
+            print("Valores encontrados:", valores)
+            print("-" * 50)
