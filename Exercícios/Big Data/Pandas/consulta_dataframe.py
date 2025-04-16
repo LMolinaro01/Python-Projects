356f4df2b0fa206455310a50463a0c15d58c692c
Leonardo Molinaro <126402616+LMolinaro01@users.noreply.github.com>
2025-04-16 16:18:31 -0300

    Exercício Pandas (Jupyter)

--- Exercícios/Big Data/Pandas/consulta_dataframe.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd #apelido
-
-meus_dados = pd.read_csv("abc.csv")
-meus_dados2 = pd.read_excel("abc.xcsx")
-meus_dados3 = pd.read_json("abc.json")
-
-meus_dados = pd.read_csv(r"C:\\Arquivos de Programas\Dados\abc.csv") #o r serve para o python não confundir a \
-meus_dados = pd.read_csv(r"C://Arquivos de Programas/Dados/abc.csv") #linux e mac
-
